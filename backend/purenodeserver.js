a944d0db81ee1c0ada39b12441aa0875582df9f1
Help <pczerokey@gmail.com>
2019-07-29 11:15:47 -0400

    Adding routes and logic

--- backend/purenodeserver.js
+++ backend/purenodeserver.js
@@ -6,17 +6,63 @@ function handler(req, res)
     // console.log(req.url);
     const parseUrl = url.parse(req.url, true);
     // console.log(parseUrl);
+    res.setHeader('x-server-date', new Date() );
+
 
     if(parseUrl.pathname === '/')
     {
         res.writeHead(200, {'Content-type': 'text/plain'});
         res.write('Hello, this is a web server!');
-        res.end();
+        return res.end();
+    }
+    else if(parseUrl.pathname === '/time')
+    {
+        res.writeHead(200, {'Content-type': 'text/plain'});
+        res.write( new Date().toString() );
+        return res.end();
+    }
+    else if(parseUrl.pathname === '/hello')
+    {
+        const name = parseUrl.query.name;
+
+        if(!name)
+        {
+            res.writeHead(404, {'Content-type': 'text/plain'});
+            return res.end();
+        }
+
+
+        res.writeHead(200, {'Content-type': 'text/plain'});
+        res.write(`Hello ${name}!`);
+        console.log(name);
+        return res.end();
+    }
+    else if(parseUrl.pathname.startsWith('/user'))
+    {
+        const regex = new RegExp('\/user\/(.+)');
+        const matches = regex.exec(parseUrl.pathname);
+
+        console.log('Got into users if');
+
+        if(!matches || !matches[1])
+        {
+            console.log('No matches');
+            res.writeHead(404, {'Content-type': 'text/plain'});
+            return res.end();
+        }
+
+        console.log('Got a user!');
+        res.writeHead(200, {'Content-type': 'text/plain'});
+        res.write(`User profile of  ${matches[1]}!`);
+        return res.end();
+
+
+
     }
     else
     {
-        res.writeHead(494, {'Content-type': 'text/plain'});
-        res.end();
+        res.writeHead(404, {'Content-type': 'text/plain'});
+        return res.end();
     }
 
 
