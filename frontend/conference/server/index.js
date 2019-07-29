6c10a5b4ce0ea2f04c8ae4823a55a17eff8f8d79
Help <pczerokey@gmail.com>
2019-07-29 16:27:04 -0400

    Back-End Creating Routes

--- frontend/conference/server/index.js
+++ frontend/conference/server/index.js
@@ -1,22 +1,10 @@
-const express  = require('express');
-
+const express = require('express');
 const app = express();
 
+const routes = require('./routes');
 
-FgBlack = "\x1b[30m";
-FgRed = "\x1b[31m";
-FgGreen = "\x1b[32m";
-FgYellow = "\x1b[33m";
-FgBlue = "\x1b[34m";
-FgMagenta = "\x1b[35m";
-FgCyan = "\x1b[36m";
-FgWhite = "\x1b[37m";
+app.use('/', routes());
 
-app.listen(3000, () => {
+app.listen(3000);
 
-    console.log(FgBlue, 'We"re listening on port 3000 baby! Everything is 200 ok! what');
-});
-
-
-
-module.exports = app;
+module.export = app;
