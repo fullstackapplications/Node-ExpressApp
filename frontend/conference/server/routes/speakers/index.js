f260ceb51759d56eb6fc872438a7adf33ae67973
Help <pczerokey@gmail.com>
2019-07-31 15:28:02 -0400

    render speakers details logic

--- frontend/conference/server/routes/speakers/index.js
+++ frontend/conference/server/routes/speakers/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const router = express.Router();
 
+const router = express.Router();
 
 module.exports = () => {
     router.get('/', (req, res, next) => {
-        return res.render('speakers'); //loads speakers in views folder
+        return res.render('speakers');
     });
 
     router.get('/:name', (req, res, next) => {
-        return res.send(`Speaker detail page for ${req.params.name}`);
+        return res.render('speakers/detail');
     });
-
+    
     return router;
 };
