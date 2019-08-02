e4090d019904b4fb1dd7a9f0f97f947a6b630593
Help <pczerokey@gmail.com>
2019-08-02 14:15:01 -0400

    Change route texts displayed

--- frontend/conference/server/routes/index.js
+++ frontend/conference/server/routes/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
-
 const router = express.Router();
-
 const speakersRoute = require('./speakers');
 const feedbackRoute = require('./feedback');
 
 module.exports = () => {
+
     router.get('/', (req, res, next) => {
-        return res.render('index');
+        // return res.send('Index');
+        return res.render('index', {
+            page: 'Home',
+        }); // loads views/index.pug
     });
 
     router.use('/speakers', speakersRoute());
+
     router.use('/feedback', feedbackRoute());
-    
+
     return router;
 };
--- frontend/conference/server/routes/speakers/index.js
+++ frontend/conference/server/routes/speakers/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
-
 const router = express.Router();
+
 
 module.exports = () => {
     router.get('/', (req, res, next) => {
-        return res.render('speakers');
+        return res.render('speakers',{
+            page: 'All Speakers',
+        }); //loads speakers in views folder
     });
 
     router.get('/:name', (req, res, next) => {
-        return res.render('speakers/detail');
+        return res.render('speakers/detail',{
+            page: req.params.name,
+        }); //loads speakers detail in views folder
     });
-    
+
     return router;
 };
