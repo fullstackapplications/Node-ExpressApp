c17af051099a91b4ecf73f25d03cc9c4ae99b7d0
Help <pczerokey@gmail.com>
2019-08-02 16:27:15 -0400

    More additions to service

--- frontend/conference/server/services/SpeakerService.js
+++ frontend/conference/server/services/SpeakerService.js
@@ -26,6 +26,18 @@ class SpeakerService
         })
     };
 
+    async getListShort() {
+        const data = await this.getData();
+
+        return data.map( (speaker) => {
+            return {
+                name: speaker.name,
+                shortname: speaker.shortname,
+                title: speaker.title
+            };
+        })
+    }
+
     async getData()
     {
         const data = await readFile(this.datafile, 'utf8');
