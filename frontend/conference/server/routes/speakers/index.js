188f294099538c34d9ec984c1aaf8ecb415da192
Help <pczerokey@gmail.com>
2019-08-05 09:45:10 -0400

    Added fetching for art and speakers with error control

--- frontend/conference/server/routes/speakers/index.js
+++ frontend/conference/server/routes/speakers/index.js
@@ -2,20 +2,40 @@ const express = require('express');
 const router = express.Router();
 
 
-module.exports = (param) => {
+module.exports = (param) =>
+{
 
-    const { speakerService } = param;
+    const {speakerService} = param;
 
-    router.get('/', async(req, res, next) => {
-        const speakersList = await speakerService.getList();
-        return res.render('speakers',{
-            page: 'All Speakers',
-            speakersList,
-        }); //loads speakers in views folder
+    router.get('/', async(req, res, next) =>
+    {
+        // const speakersList = await speakerService.getList();
+
+        try
+        {
+            const promises = [];
+
+            promises.push(speakerService.getList());
+            promises.push(speakerService.getAllArtwork());
+
+            const results = await Promise.all(promises);
+
+            return res.render('speakers', {
+                page: 'All Speakers',
+                speakersList: results[0],
+                artWork: results[1],
+            }); //loads speakers in views folder
+        }
+        catch(error)
+        {
+            return error;
+        }
+
     });
 
-    router.get('/:name', (req, res, next) => {
-        return res.render('speakers/detail',{
+    router.get('/:name', (req, res, next) =>
+    {
+        return res.render('speakers/detail', {
             page: req.params.name,
         }); //loads speakers detail in views folder
     });
