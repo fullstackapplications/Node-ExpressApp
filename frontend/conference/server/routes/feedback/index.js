388e7e188e91abdd977bd12432eeb010d638ebed
Help <pczerokey@gmail.com>
2019-08-05 16:05:28 -0400

    feedback page handler

--- frontend/conference/server/routes/feedback/index.js
+++ frontend/conference/server/routes/feedback/index.js
@@ -2,13 +2,62 @@ const express = require('express');
 const router = express.Router();
 
 
-module.exports = () => {
-    router.get('/', (req, res, next) => {
-        return res.send('Feedback');
+module.exports = (param) =>
+{
+
+    const {speakerService} = param;
+
+    router.get('/', async(req, res, next) =>
+    {
+        // const speakersList = await speakerService.getList();
+
+        try
+        {
+            const promises = [];
+
+            promises.push(speakerService.getList());
+            promises.push(speakerService.getAllArtwork());
+
+            const results = await Promise.all(promises);
+
+            return res.render('speakers', {
+                page: 'All Speakers',
+                speakersList: results[0],
+                artWork: results[1],
+            }); //loads speakers in views folder
+        }
+        catch(error)
+        {
+            return error;
+        }
+
     });
 
-    router.post('/', (req, res, next) => {
-        return res.send(`Form sent!`);
+    router.get('/:name', async(req, res, next) =>
+    {
+        try
+        {
+            const promises = [];
+            promises.push(speakerService.getSpeaker(req.params.name));
+            promises.push(speakerService.getArtworkForSpeaker(req.params.name));
+            const results = await Promise.all(promises);
+
+            if(!results[0])
+            {
+                return next();
+            }
+
+            return res.render('speakers/detail', {
+                page: req.params.name,
+                speaker: results[0],
+                artWork: results[1],
+            }); //loads speakers detail in views folder
+        }
+        catch(error)
+        {
+            return next(error);
+        }
+
     });
 
     return router;
