b420be87a81b227a62093590b13b60eef86f1394
Help <pczerokey@gmail.com>
2019-08-05 15:59:31 -0400

    Speaker service additions

--- frontend/conference/server/services/SpeakerService.js
+++ frontend/conference/server/services/SpeakerService.js
@@ -1,117 +1,74 @@
 const fs = require('fs');
 const util = require('util');
-// const speakers = require('../data/speakers');
 
 const readFile = util.promisify(fs.readFile);
 
-class SpeakerService
-{
-    constructor(datafile)
-    {
-        this.datafile = datafile;
+class SpeakerService {
+  constructor(datafile) {
+    this.datafile = datafile;
+  }
+
+  async getNames() {
+    const data = await this.getData();
+
+    return data.map((speaker) => {
+      return { name: speaker.name, shortname: speaker.shortname };
+    });
+  }
+
+  async getListShort() {
+    const data = await this.getData();
+    return data.map((speaker) => {
+      return { name: speaker.name, shortname: speaker.shortname, title: speaker.title };
+    });
+  }
+
+  async getList() {
+    const data = await this.getData();
+    return data.map((speaker) => {
+      return { name: speaker.name, shortname: speaker.shortname, title: speaker.title, summary: speaker.summary };
+    });
+  }
+
+  async getAllArtwork() {
+    const data = await this.getData();
+    const artwork = data.reduce((acc, elm) => {
+      if(elm.artwork) {
+        acc = [...acc, ...elm.artwork];
+      }
+      return acc;
+    }, []);
+    return artwork;
+  }
+
+  async getSpeaker(shortname) {
+    const data = await this.getData();
+    const speaker = data.find((speaker) => {
+      return speaker.shortname === shortname;
+    });
+    if(!speaker) return null;
+    return {
+      title: speaker.title,
+      name: speaker.name,
+      shortname: speaker.shortname,
+      description: speaker.description,
     }
+  }
 
-    async getNames()
-    {
-        const data = await this.getData();
-        if(!data)
-        {
-            return []
-        }
-        return data.map((speaker) =>
-        {
-            return {
-                name: speaker.name,
-                shortname: speaker.shortname,
-            };
-        })
-    };
+  async getArtworkForSpeaker(shortname) {
+    const data = await this.getData();
+    const speaker = data.find((speaker) => {
+      return speaker.shortname === shortname;
+    });
+    if(!speaker || !speaker.artwork) return null;
+    return speaker.artwork;
+  }
 
-    async getListShort()
-    {
-        const data = await this.getData();
-
-        return data.map((speaker) =>
-        {
-            return {
-                name: speaker.name,
-                shortname: speaker.shortname,
-                title: speaker.title
-            };
-        })
-    }
-
-    async getList()
-    {
-        const data = await this.getData();
-
-        return data.map((speaker) =>
-        {
-            return {
-                name: speaker.name,
-                shortname: speaker.shortname,
-                title: speaker.title,
-                summary: speaker.summary
-            };
-        })
-    }
-
-    async getAllArtwork()
-    {
-        const data = await this.getData();
-        const artwork = data.reduce((accumulator, element) =>
-        {
-            if(element.artwork)
-            {
-                accumulator = [...accumulator, ...element.artwork];
-            }
-            return accumulator;
-        }, []);
-        return artwork;
-    }
-
-    async getSpeaker(shortname)
-    {
-        const data = await this.getData();
-        const speaker = data.find((speaker) =>
-        {
-            return speaker.shortname == shortname;
-        });
-        if(!speaker)
-        {
-            return null;
-        }
-        return {
-            title: speaker.title,
-            name: speaker.name,
-            shortname: speaker.shortname,
-            description: speaker.description,
-        }
-    }
-
-    async getArtworkForSpeaker(shortname)
-    {
-        const data = await this.getData();
-        const speaker = data.find((speaker) =>
-        {
-            return speaker.shortname === shortname;
-        });
-        if(!speaker || !speaker.artwork)
-        {
-            return null;
-        }
-        return speaker.artwork;
-    }
-
-    async getData()
-    {
-        const data = await readFile(this.datafile, 'utf8');
-        if(!data)
-        {
-            return []
-        }
-        return JSON.parse(data).speakers;
-    }
+  async getData() {
+    const data = await readFile(this.datafile, 'utf8');
+    if (!data) return [];
+    return JSON.parse(data).speakers;
+  }
 }
 
 module.exports = SpeakerService;
